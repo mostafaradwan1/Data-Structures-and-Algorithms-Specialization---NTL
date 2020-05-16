c2c468cd9eacde57fcb5d05def2f8847acab2480
mostafaradwan1 <mostafaradwan1001@gmail.com>
2020-05-16 15:43:06 +0200

    bfs week 3 algo on graphs

--- algorithms on graphs/week3_paths1/1_bfs/bfs.py
+++ algorithms on graphs/week3_paths1/1_bfs/bfs.py
@@ -5,7 +5,20 @@ import queue
 
 def distance(adj, s, t):
     #write your code here
+    dist = [len(adj)] * len(adj)
+    dist[s] = 0
+    queue = []
+    queue.append(s)
+    while queue:
+        u = queue.pop(0)
+        for v in adj[u]:
+            if dist[v] == len(adj):
+                queue.append(v)
+                dist[v] = dist[u] + 1
+    if dist[t] != len(adj):
+        return dist[t]
     return -1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
